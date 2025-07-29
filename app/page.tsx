6472a77c2f5878f281d5c573f372540c995cd596
Nane <gemine_nicolas@hotmail.com>
2025-07-29 13:36:56 +0200

    ProductCart

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import Image from 'next/image';
+import Link from 'next/link';
+import ProductCard from './components/ProductCard';
 
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <main>
+      <div>Hello World</div>
+      <Link href='/users'>Users</Link>
+      <ProductCard />
+    </main>
+  );
 }
